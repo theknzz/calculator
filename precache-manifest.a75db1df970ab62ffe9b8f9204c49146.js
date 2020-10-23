221434aaaa21a05de94480e11d18c1af35d4a8bd
theknzz <joaquimpersonalmail@isec.pt>
2020-10-23 17:44:43 +0100

    Updates

---
+++ precache-manifest.a75db1df970ab62ffe9b8f9204c49146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231c91f9050b559df923ff95645459f8",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7c3ce7c47c03817b81f9",
+    "url": "/calculator/static/css/main.9f03c6a3.chunk.css"
+  },
+  {
+    "revision": "b48097278a9332736acb",
+    "url": "/calculator/static/js/2.500ae9ba.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/calculator/static/js/2.500ae9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c3ce7c47c03817b81f9",
+    "url": "/calculator/static/js/main.7baeef63.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
